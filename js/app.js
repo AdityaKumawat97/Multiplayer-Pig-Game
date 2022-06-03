113aed05df5ab6e2faf7b77e623f728a48f8b96a
kumartul <kumartul001@gmail.com>
2022-06-03 19:49:39 +0530

    [Refactor]: Simplify a conditional and fix a typo

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -63,7 +63,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 function nextPlayer() {
     //Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     roundScore = 0;
 
     document.getElementById('current-0').textContent = '0';
